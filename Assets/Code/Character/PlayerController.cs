6c675401cdf9dd06c29e3c839cdd2037d23cbf75
David López López <dlopez.informatico@gmail.com>
2015-10-24 00:50:45 +0200

    WIP PlayerController

---
+++ Assets/Code/Character/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+	public float MoveForce;
+	public float JumpForce;
+	
+	private Rigidbody2D rigidBody;
+	private bool canJump;
+	
+	void Awake ()
+	{
+		InputManager.OnMove += HandleMove;
+		InputManager.OnJump += HandleJump;
+		InputManager.OnPieceIn += HandlePieceIn;
+		InputManager.OnPieceOut += HandlePieceOut;
+		
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void OnDestroy ()
+	{
+		InputManager.OnMove -= HandleMove;
+		InputManager.OnJump -= HandleJump;
+		InputManager.OnPieceIn -= HandlePieceIn;
+		InputManager.OnPieceOut -= HandlePieceOut;
+	}
+	
+	void OnCollisionEnter2D(Collision2D collider)
+	{
+		if(collider.gameObject.tag.Equals("Floor"))
+		{
+			canJump = true;
+		}
+	}
+	
+	private void HandleMove(float direction)
+	{
+		rigidBody.velocity = new Vector2(direction * MoveForce, rigidBody.velocity.y);
+	}
+	
+	private void HandleJump()
+	{
+		if (canJump) 
+		{
+			rigidBody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+			canJump = false;
+		}
+	}
+	
+	private void HandlePieceIn(Piece piece)
+	{
+		
+	}
+	
+	private void HandlePieceOut(Piece piece)
+	{
+		
+	}
+}
