0dd84e9f2df5fd0b585ec8f71845ed5a09d86e0c
MarcEstVar <denzelnihlus@gmail.com>
2015-10-23 22:23:25 +0200

    UI WIP bocadillo prefab

---
+++ Assets/Code/Bocadillo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Bocadillo : MonoBehaviour {
+
+	public string text;
+
+	// Use this for initialization
+	void Start () {
+		//text=this.GetComponentInChildren<Text>().text;
+		if(text!=""){
+			this.CambiarTexto(text);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CambiarTexto(string nuevotexto){
+		this.GetComponentInChildren<Text>().text=nuevotexto;
+	}
+}
