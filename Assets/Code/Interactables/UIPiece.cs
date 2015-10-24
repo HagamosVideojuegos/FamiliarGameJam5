f2774e2ff55ea6f9b9d2138f19741f27b7baf9d5
David López López <dlopez.informatico@gmail.com>
2015-10-24 16:16:19 +0200

    Refactoring

--- Assets/Code/Interactables/UIPiece.cs
+++ Assets/Code/Interactables/UIPiece.cs
@@ -1,40 +1,41 @@
-﻿public class UIPiece : InteractableObjectUI
+﻿using UnityEngine;
+
+public class UIPiece : InteractableObjectUI
 {
 	public int Weight;
-	public PieceType pieceType;
-
+	public Piece piece;
     protected override void Interact()
     {
         GameManager.Instance.player.Weight += Weight;
-		switch (pieceType)
+		switch (piece.pieceType)
 		{
 			case PieceType.Arm:
 			switch(GameManager.Instance.player.pieces.arms)
 			{
-				case 0:
-				GameManager.Instance.player.pieces.RightArm.SetActive(true);
-				break;
 				case 1:
-				GameManager.Instance.player.pieces.LeftArm.SetActive(true);
+				GameManager.Instance.player.pieces.RightArm.SetActive(false);
+				break;
+				case 2:
+				GameManager.Instance.player.pieces.LeftArm.SetActive(false);
 				break;
 			}
 			break;
 			case PieceType.Head:
-			GameManager.Instance.player.pieces.Head.SetActive(true);
+			GameManager.Instance.player.pieces.Head.SetActive(false);
 			break;
 			case PieceType.Leg:
 			switch(GameManager.Instance.player.pieces.legs)
 			{
-				case 0:
-				GameManager.Instance.player.pieces.RightLeg.SetActive(true);
-				break;
 				case 1:
-				GameManager.Instance.player.pieces.LeftLeg.SetActive(true);
+				GameManager.Instance.player.pieces.RightLeg.SetActive(false);
+				break;
+				case 2:
+				GameManager.Instance.player.pieces.LeftLeg.SetActive(false);
 				break;
 			}
 			break;
 		}
-		GameManager.Instance.player.ActivatePiece(pieceType);
-		Destroy(gameObject);
+		GameManager.Instance.player.DeactivatePiece(piece.pieceType);
+		Instantiate(piece, GameManager.Instance.player.transform.position, new Quaternion(0, 0, 0 ,0));
     }
 }
