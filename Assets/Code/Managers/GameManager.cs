4222e5edd17c37e860eeb6701cde7705b71baf8a
David López López <dlopez.informatico@gmail.com>
2015-10-24 13:30:12 +0200

    Hide PlayerController in Inspector

--- Assets/Code/Managers/GameManager.cs
+++ Assets/Code/Managers/GameManager.cs
@@ -1,4 +1,7 @@
-﻿public class GameManager : Singleton<GameManager>
+﻿using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
 {
+	[HideInInspector]
 	public PlayerController player;
 }
