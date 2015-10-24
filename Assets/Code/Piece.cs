3d192f7a7df0e4ecc74e74e78fd86444b4b1b5f2
David López López <dlopez.informatico@gmail.com>
2015-10-24 14:07:51 +0200

    WIP Piece object

--- Assets/Code/Piece.cs
+++ Assets/Code/Piece.cs
@@ -1,15 +1,9 @@
 ﻿public class Piece : InteractableObjectTrigger
 {
-	public bool mounted;
 	public int Weight;
 
     protected override void Interact()
     {
-        if (mounted)
-		{
-			//TODO: Make a new clone piece and put out
-		} else {
-			//TODO: Destroy this and activate the equal piece in the player
-		}
+        GameManager.Instance.player.Weight += Weight;
     }
 }
