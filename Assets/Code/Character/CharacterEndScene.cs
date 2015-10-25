4c17bc812fa45a7435e4300e15a26ccffc6e1947
David López López <dlopez.informatico@gmail.com>
2015-10-25 16:33:34 +0100

    End scene

---
+++ Assets/Code/Character/CharacterEndScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CharacterEndScene : MonoBehaviour
+{
+	private Animator animator;
+
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponentInChildren<Animator>();
+		animator.SetInteger("legs", 0);
+	}
+}
---
+++ Assets/Code/UI/UIRestartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIRestartGame : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.LoadLevel(0);
+    }
+}
