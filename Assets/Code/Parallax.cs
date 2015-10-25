cc58548d93b31a96198889cd123d346ad9089a74
MarcEstVar <denzelnihlus@gmail.com>
2015-10-25 12:21:45 +0100

    Parallax Camara

---
+++ Assets/Code/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public SpriteRenderer fondo;
+	private Vector3 lastPosition,currentPosition,deltaPosition;
+
+	// Use this for initialization
+	void Start () {
+		if(fondo==null){
+			GameObject.Find("Backgrounds");
+		}
+		this.lastPosition=this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentPosition=this.transform.position;
+		deltaPosition=lastPosition-currentPosition;
+		lastPosition=currentPosition;
+		Debug.Log(deltaPosition);
+		fondo.transform.Translate(deltaPosition * 0.1f);
+	}
+}
