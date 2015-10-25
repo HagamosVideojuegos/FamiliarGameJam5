9f919c3f6d1f8240bc1da91ddcf09810519db636
MarcEstVar <denzelnihlus@gmail.com>
2015-10-25 17:22:30 +0100

    Cabina Script

--- Assets/Code/Interactables/Cab.cs
+++ Assets/Code/Interactables/Cab.cs
@@ -4,18 +4,22 @@ public class Cab : InteractableObjectTrigger
 {
 	public AudioClip nextLevelAudio,errorLevelAudio;
 
-	private GameObject sound;
+	private GameObject soundexit,soundfail;
 
     protected override void Interact()
     {
         if(GameManager.Instance.player.pieces.head)
 		{
 			GameManager.Instance.player.OnDestroy();
-			AudioManager.Instance.PlaySound(nextLevelAudio);
+			if(soundexit==null){
+				soundexit=AudioManager.Instance.PlaySound(nextLevelAudio);
+			}
 			Invoke("NextLevel", nextLevelAudio.length);
 		} else {
 			GameManager.Instance.player.NoPiece(PieceType.Head);
-			AudioManager.Instance.PlaySound(errorLevelAudio);
+			if(soundfail==null){
+				soundfail=AudioManager.Instance.PlaySound(errorLevelAudio);
+			}
 		}
     }
 	
