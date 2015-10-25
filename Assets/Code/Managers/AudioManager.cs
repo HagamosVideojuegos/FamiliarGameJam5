476e78e073c4f07052650fa72bd5d34715437612
David López López <dlopez.informatico@gmail.com>
2015-10-25 15:00:10 +0100

    audiomanager wip?

---
+++ Assets/Code/Managers/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AudioManager : Singleton<MonoBehaviour>
+{
+	private GameObject g;
+	
+	void Start ()
+	{
+		g = new GameObject();
+		g.AddComponent<AudioSource>();
+	}
+	
+	public void PlaySound(AudioClip audio)
+	{
+		GameObject.Instantiate(g).GetComponent<AudioSource>().clip = audio;
+	}
+}
