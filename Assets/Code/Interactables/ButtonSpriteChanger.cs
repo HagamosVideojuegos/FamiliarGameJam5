27b88c934393ece91953a480e10be129906aa28a
MarcEstVar <denzelnihlus@gmail.com>
2015-10-25 11:41:37 +0100

    Button And Hanger Prefab

---
+++ Assets/Code/Interactables/ButtonSpriteChanger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSpriteChanger : MonoBehaviour {
+
+	public Sprite activated,deactivated;
+
+	private SpriteRenderer spriterender;
+	private Button selfInstance;
+
+	// Use this for initialization
+	void Start () {
+		spriterender=this.GetComponent<SpriteRenderer>();
+		selfInstance=this.GetComponent<Button>();
+		Button.OnActivate+=Activate;
+		Button.OnDeactivate+=Deactivate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Activate (Button button)
+	{
+		if(button==this.selfInstance){
+			spriterender.sprite=activated;
+		}
+	}
+
+	void Deactivate (Button button)
+	{
+		if(button==this.selfInstance){
+			spriterender.sprite=deactivated;
+		}
+	}
+}
