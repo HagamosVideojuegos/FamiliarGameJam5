a81f4fafd1067ac54bfca47aa2d1e2901a327d8a
MarcEstVar <denzelnihlus@gmail.com>
2015-10-25 16:41:03 +0100

    Sounds to Finish

--- Assets/Code/Interactables/Cab.cs
+++ Assets/Code/Interactables/Cab.cs
@@ -2,15 +2,21 @@
 
 public class Cab : InteractableObjectTrigger
 {
+	public AudioClip nextLevelAudio,errorLevelAudio;
+
+	private GameObject sound;
+
     protected override void Interact()
     {
         if(GameManager.Instance.player.pieces.head)
 		{
 			//TODO: SFX Next Level
+			AudioManager.Instance.PlaySound(nextLevelAudio);
 			Invoke("NextLevel", 2f);
 		} else {
 			GameManager.Instance.player.NoPiece(PieceType.Head);
 			//TODO: SFX Error
+			AudioManager.Instance.PlaySound(errorLevelAudio);
 		}
     }
 	
