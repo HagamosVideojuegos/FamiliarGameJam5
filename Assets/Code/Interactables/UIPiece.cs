3eeb76af0bf3dafa6b9f56628d9acbb77d07af09
MarcEstVar <denzelnihlus@gmail.com>
2015-10-25 16:53:26 +0100

    Sounds to buttons

--- Assets/Code/Interactables/UIPiece.cs
+++ Assets/Code/Interactables/UIPiece.cs
@@ -4,14 +4,15 @@ public class UIPiece : InteractableObjectUI
 {
 	public int Weight;
 	public Piece piece;
+	public AudioClip okSound,failSound;
     protected override void Interact()
     {
 		if(GameManager.Instance.player.DeactivatePiece(piece.pieceType))
 		{
 			Instantiate(piece, GameManager.Instance.player.transform.position, new Quaternion(0, 0, 0 ,0));
-			//TODO: Button OK Sound
+			if(okSound!=null)AudioManager.Instance.PlaySound(okSound);
 		} else {
-			//TODO: Button fail Sound
+			if(failSound!=null)AudioManager.Instance.PlaySound(failSound);
 		}
 			
     }
